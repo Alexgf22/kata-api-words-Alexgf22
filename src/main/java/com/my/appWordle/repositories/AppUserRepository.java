8c611a9a71dc9cbcb20492f5ec0bf572962fb8dd
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-12-09 16:59:46 +0100

    Empezando la implementación de la parte de seguridad

---
+++ src/main/java/com/my/appWordle/repositories/AppUserRepository.java
@@ -0,0 +1,9 @@
+package com.my.appWordle.repositories;
+
+import com.my.appWordle.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
+
---
+++ src/main/java/com/my/appWordle/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.my.appWordle.services;
+
+import com.my.appWordle.models.AppUser;
+import com.my.appWordle.repositories.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username);
+        if (appUser != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(appUser.getUsername())
+                    .password(appUser.getPassword())
+                    .roles("USER")
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("Usuario no encontrado: " + username);
+        }
+    }
+}
